e0d72e63dc6da24f0aaf39d04795244fdc1b9690
brett <bperry13@asu.edu>
2022-01-26 18:53:32 -0600

    First commit on this project

---
+++ PerryLKM.c
@@ -0,0 +1,18 @@
+/**
+ * write a LKM for the Linux kernel that displays the following details for all the
+ * processes whose PID is greater than an integer given by the user as a module parameter:
+ * PROCESS NAME, PID, STATE, PRIORITY, STATIC-PRIORITY, NORMAL-PRIORITY 
+ *
+ * Completion time:
+ *
+ * @author
+ * @version
+ */
+
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
+#include <linux/list.h>
+
+int main() {
+  
+}
