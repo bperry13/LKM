5e7e063226cd5f0347fd49a53b35b61831bda8ae
brett <bperry13@asu.edu>
2022-01-30 08:43:54 -0600

    MVP works

--- PerryLKM.c
+++ PerryLKM.c
@@ -3,16 +3,34 @@
  * processes whose PID is greater than an integer given by the user as a module parameter:
  * PROCESS NAME, PID, STATE, PRIORITY, STATIC-PRIORITY, NORMAL-PRIORITY 
  *
- * Completion time:
+ * kernel version 5.11.0-46-generic
  *
- * @author
- * @version
+ * Completion time: 3 hours
+ * //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
+ * @author Brett Perry
+ * @version 1.31.22
  */
 
-#include <linux/sched.h>
-#include <linux/sched/signal.h>
-#include <linux/list.h>
+#include <linux/init.h>
+#include <linux/module.h>  
+#include <linux/kernel.h> 
 
-int main() {
+
+static int PerryLKM_init(void) 
+{
+	printk(KERN_INFO "Loading Module\n");
   
+	return 0;
 }
+
+static void PerryLKM_exit(void) 
+{
+	printk(KERN_INFO "Removing Module\n");
+}
+
+module_init( PerryLKM_init );
+module_exit( PerryLKM_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("LKM");
+MODULE_AUTHOR("BRETTPERRY");
